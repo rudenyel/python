f605802179130ed8d983029249f11244f976cbd8
rudenyel <119690451+rudenyel@users.noreply.github.com>
2022-12-30 00:20:55 +0100

    Generators

---
+++ HW_06.py
@@ -0,0 +1,20 @@
+def get_even_or_odd_numbers(stop, only_even):
+    return [number for number in range(stop) if number % 2 != only_even]
+
+
+def search_words(phrase, words):
+    return [word for word in words if phrase in word]
+
+
+def flatten(sequence):
+    yield from [item for subsequence in sequence for item in subsequence]
+
+
+print(get_even_or_odd_numbers(10, True))
+
+print(search_words('he', ['hello', 'orange', 'phenomenon']))
+
+generator = flatten([[1, 2], [], [3, 4, 5]])
+print(next(generator))
+print(next(generator))
+print(next(generator))
